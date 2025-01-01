b0ec0806fcf2b7aa8f6eb40d17e5b6dfdedb4275
Arjun <Arjun's computer>
2025-01-01 15:40:38 -0800

    new pedro pathing is set up

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.constants;
+
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.localization.Localizers;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+// This acts as a method of updating FollowerConstants without direct access to it
+public class FConstants { //This is how we change Follower Constants
+    static {
+        // Select our localizer
+        FollowerConstants.localizers = Localizers.TWO_WHEEL;
+
+// hardware map names
+        FollowerConstants.leftFrontMotorName = "leftFront";
+        FollowerConstants.leftRearMotorName = "leftRear";
+        FollowerConstants.rightFrontMotorName = "rightFront";
+        FollowerConstants.rightRearMotorName = "rightRear";
+
+        FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
+        FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
+        FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
+        FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
+
+        // We can change the value of any variable/constant of FollowerConstants
+        FollowerConstants.mass = 10; // In kg
+
+    }
+}
